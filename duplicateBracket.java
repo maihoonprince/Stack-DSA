fa097301576d584cd3084a776d556741d6aa105e
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-05-12 06:20:07 +0530

    Add files via upload

---
+++ duplicateBracket.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class duplicateBracket {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        Stack <Character> st = new Stack<>();
+
+         for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == ')'){
+                if(st.peek() == '('){
+                    System.out.println(true);
+                    return;
+                }
+                else{
+                    while(st.peek() != '('){
+                        st.pop();
+                    }
+                    st.pop();
+                }
+            }
+            else{
+                st.push(ch);
+            }
+         }
+         System.out.println(false);
+    }
+}
